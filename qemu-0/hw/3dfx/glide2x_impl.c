c9497ef326b158ae0d48ae18faeafccdfc5b28c5
KJ Liew <liewkj@yahoo.com>
2020-03-27 10:46:20 -0700

    Update license and years

--- qemu-0/hw/3dfx/glide2x_impl.c
+++ qemu-0/hw/3dfx/glide2x_impl.c
@@ -1,20 +1,21 @@
 /*
- * QEMU 3Dfx Glide Pass-Through 
+ * QEMU 3Dfx Glide Pass-Through
  *
- *  Copyright (c) 2018
+ *  Copyright (c) 2018-2020
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either
  * version 2 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public
+ * License along with this library;
+ * if not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "qemu/osdep.h"
