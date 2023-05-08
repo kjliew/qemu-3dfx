74cb10fd5f84176e4587bd486322138d37c961fb
KJ Liew <liewkj@yahoo.com>
2023-05-07 19:01:28 -0700

    Add WGL extensions blessing

--- wrappers/mesa/src/wrapgl32.c
+++ wrappers/mesa/src/wrapgl32.c
@@ -399,7 +399,7 @@ static FILE * opt_fopen(void)
     return ret;
 }
 
-static void fltrxstr(const char *xstr, size_t len, const int xstrYear)
+static void fltrxstr(const char *xstr, size_t len, const char *bless)
 {
 #define MAX_XSTR 128
     char *str = (char *)xstr, *tmp = (char *)&fbtm[(MGLFBT_SIZE - (3*PAGE_SIZE)) >> 2];
@@ -432,7 +432,7 @@ static void fltrxstr(const char *xstr, size_t len, const int xstrYear)
         }
         while(fgets(line, MAX_XSTR, f)) {
             slen = strnlen(line, MAX_XSTR) - 1;
-            if (xstrYear && !memcmp(line, "+GL_", 4)) {
+            if (bless && !memcmp(line, bless, strlen(bless))) {
                 line[slen] = '\0';
                 OHST_DMESG("..blessing %s", &line[1]);
                 memcpy(tmp, &line[1], slen - 1);
@@ -5404,7 +5404,7 @@ uint8_t * PT_CALL glGetString(uint32_t arg0) {
     pt0 = (uint32_t *)pt[0]; *pt0 = FEnum_glGetString;
     fifoOutData(0, (uint32_t)cstrTbl[sel], cstrsz[sel]);
     if (sel == 0x03U)
-        fltrxstr(cstrTbl[sel], cstrsz[sel], cfg.xstrYear);
+        fltrxstr(cstrTbl[sel], cstrsz[sel], (cfg.xstrYear)? "+GL_":0);
     //DPRINTF("%s [ %04x ]", cstrTbl[sel], arg0);
     return (uint8_t *)cstrTbl[sel];
 }
@@ -16569,7 +16569,7 @@ static uint32_t PT_CALL wglGetExtensionsStringARB(uint32_t arg0)
     WGL_FUNCP("wglGetExtensionsStringARB");
     ptm[0xFDC >> 2] = MESAGL_MAGIC;
     strncpy((char *)wstrtbl[0], (char *)&mfifo[(MGLSHM_SIZE - PAGE_SIZE) >> 2], PAGE_SIZE);
-    fltrxstr(wstrtbl[0], PAGE_SIZE - 1, 0);
+    fltrxstr(wstrtbl[0], PAGE_SIZE - 1, "+WGL_");
     //DPRINTF("GetExtensionsStringARB %s", wstrtbl[0]);
     return (uint32_t)wstrtbl[0];
 }
