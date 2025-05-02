82333cc94fe77f19017f3bdc3f62d2a2593a2276
KJ Liew <liewkj@yahoo.com>
2025-05-02 03:57:37 -0700

    Mapped BO heuristic requires kernel >= 6.13

--- qemu-1/hw/mesa/mglcntx_sdlgl.c
+++ qemu-1/hw/mesa/mglcntx_sdlgl.c
@@ -57,7 +57,11 @@ int MGLUpdateGuestBufo(mapbufo_t *bufo, int add) { return 0; }
 
 int MGLUpdateGuestBufo(mapbufo_t *bufo, int add)
 {
-    int ret = (GetBufOAccelEN() || (bufo && bufo->tgt == GL_PIXEL_UNPACK_BUFFER))? kvm_enabled():0;
+    int ret = (GetBufOAccelEN()
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+            || (bufo && bufo->tgt == GL_PIXEL_UNPACK_BUFFER)
+#endif
+            )? kvm_enabled():0;
 
     if (ret && bufo) {
         bufo->lvl = (add)? MapBufObjGpa(bufo):0;
